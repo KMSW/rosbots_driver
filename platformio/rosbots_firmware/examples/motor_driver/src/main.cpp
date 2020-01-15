9b634c380c0ceeb05d7921ad0631cf395eb47721
Kai Mueller <kai@kmsoftware.de>
2020-01-15 05:03:07 +0100

    Changed to Motordriver Elegoo L298N

--- platformio/rosbots_firmware/examples/motor_driver/src/main.cpp
+++ platformio/rosbots_firmware/examples/motor_driver/src/main.cpp
@@ -13,34 +13,39 @@ ros::NodeHandle nh;
 #define LED_BUILTIN 13
 #endif
 
-#define M_LEFT_PWM 6
-#define M_LEFT_FR 7
+#define M_LEFT_PWM 10
+#define M_LEFT_IN1 9
+#define M_LEFT_IN2 8
+
 #define M_RIGHT_PWM 5
-#define M_RIGHT_FR 4
+#define M_RIGHT_IN1 7
+#define M_RIGHT_IN2 6
 
 void turnWheel( const std_msgs::Float32 &wheel_power,
                 unsigned int pwm_pin,
-                unsigned int fr_pin ) {
+                unsigned int in1_pin, 
+                unsigned int in2_pin ) {
     float factor = max(min(wheel_power.data, 1.0f), -1.0f);
     if( factor >= 0 ) {
-        digitalWrite(fr_pin, LOW);
+        digitalWrite(in1_pin, HIGH);digitalWrite(in2_pin, LOW);
         analogWrite(pwm_pin, (unsigned int)(255 * factor));
     } else {
-        digitalWrite(fr_pin, HIGH);
+        digitalWrite(in2_pin, HIGH);digitalWrite(in1_pin, LOW);
         analogWrite(pwm_pin, (unsigned int)(255 * (1.0f + factor)));
     }   
 }
+
 void rightWheelCb( const std_msgs::Float32 &wheel_power ) {
     //nh.loginfo("Wheel Power - Right");
     //char result[8];
     //dtostrf(wheel_power.data, 6, 2, result); 
     //nh.loginfo(result);
-    turnWheel( wheel_power, M_RIGHT_PWM, M_RIGHT_FR );
+    turnWheel( wheel_power, M_RIGHT_PWM, M_RIGHT_IN1, M_RIGHT_IN2 );
 }
 void leftWheelCb( const std_msgs::Float32 &wheel_power ) {
-    //nh.loginfo("Wheel Power - Left");
-    turnWheel( wheel_power, M_LEFT_PWM, M_LEFT_FR );
+    turnWheel( wheel_power, M_LEFT_PWM, M_LEFT_IN1, M_LEFT_IN2 );
 }
+
 ros::Subscriber<std_msgs::Float32> sub_right("wheel_power_right",
                                             &rightWheelCb );
 ros::Subscriber<std_msgs::Float32> sub_left("wheel_power_left",
@@ -54,15 +59,19 @@ void setup()
   digitalWrite(LED_BUILTIN, HIGH);
 
   pinMode(M_LEFT_PWM, OUTPUT);
-  pinMode(M_LEFT_FR, OUTPUT);
+  pinMode(M_LEFT_IN1, OUTPUT);
+  pinMode(M_LEFT_IN2, OUTPUT);
   pinMode(M_RIGHT_PWM, OUTPUT);
-  pinMode(M_RIGHT_FR, OUTPUT);
+  pinMode(M_RIGHT_IN1, OUTPUT);
+  pinMode(M_RIGHT_IN2, OUTPUT);
 
   // Init motors to stop
-  digitalWrite(M_LEFT_FR, LOW);
-  digitalWrite(M_RIGHT_FR, LOW);
-  analogWrite(M_LEFT_PWM, 0);
-  analogWrite(M_RIGHT_PWM, 0);
+  digitalWrite(M_LEFT_IN1, HIGH);
+  digitalWrite(M_LEFT_IN2, LOW);
+  digitalWrite(M_RIGHT_IN1, HIGH);
+  digitalWrite(M_RIGHT_IN2, LOW);
+  analogWrite(M_LEFT_PWM, 128);
+  analogWrite(M_RIGHT_PWM, 128);
 
   nh.initNode();
   nh.subscribe(sub_right);
